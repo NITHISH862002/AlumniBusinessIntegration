d4a888fffe299da9170e4022966b6e7cbc7b875a
Mohamed Aslam <mohamedaslammn.2003@gmail.com>
2023-07-15 13:50:33 +0530

    API Setup

---
+++ index.js
@@ -0,0 +1,42 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { Server as httpServer } from "http";
+// import socket from "./socket/index.js";
+
+import config_db from "./config/dbconfig.js";
+
+dotenv.config();
+config_db(process.env.URI);
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+if (process.env.NODE_ENV === "DEVELOPMENT") {
+  console.log("Development mode".yellow);
+  app.get("/", (req, res) => {
+    res.send("ALUMNI INTEGRATOR API");
+  });
+} else {
+  console.log("Production mode".green);
+  const root = path.join(__dirname, "client", "build");
+  app.use(express.static(root));
+  app.get("*", (req, res) => {
+    res.sendFile("index.html", { root });
+  });
+}
+
+// app.use(notFound);
+// app.use(errorHandler);
+
+const server = httpServer(app);
+
+// socket(server);
+
+server.listen(process.env.PORT, () => {
+  console.log(
+    `Server is running on port ${process.env.PORT}`.black.bgGreen.bold
+  );
+});
